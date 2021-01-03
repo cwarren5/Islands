084f1dfc4101569f6d739af1d2152c9770a9a575
Cliff <cliff@cliffwarren.com>
2021-01-03 08:09:40 -0500

    MVP Build (Without Narrative)

---
+++ Assets/MenuSelection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour
+{
+    [SerializeField] private int correspondingScene = 2;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(correspondingScene);
+    }
+}
